8a4f3cc8588e125a876bdc5918ea4bf12ce4cfbb
user <chipsikchannel@gmail.com>
2023-02-13 16:55:54 +0300

    Добавлена страница-шаблон с описанием для каждого аниме

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index),
-    path("demon-slayer", views.demonslayer)
+    path("demon-slayer", views.demonslayer),
+    path("mob-psycho", views.mobpsycho),
+    path("chainsaw-man", views.chainsawman),
 ]
--- main/views.py
+++ main/views.py
@@ -5,4 +5,10 @@ def index(request):
     return render(request, "main/index.html")
 
 def demonslayer(request):
-    return render(request, "main/demonslayer.html")    
+    return render(request, "main/demonslayer.html")
+
+def mobpsycho(request):
+    return render(request, "main/mob.html")
+
+def chainsawman(request):
+    return render(request, "main/chainsawman.html")
