5e7a448206fb84be98af0fd7cc8f5933c3336a80
user <chipsikchannel@gmail.com>
2023-02-27 22:48:19 +0300

    Изменены модели, добавлена связующая таблица.

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Genre, Anime
+from .models import Genre, Anime, Jumbotron, AnimeGenre
 
 admin.site.register(Anime)
 admin.site.register(Genre)
-
-
+admin.site.register(Jumbotron)
+admin.site.register(AnimeGenre)
--- main/models.py
+++ main/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
 class Genre(models.Model):
     name_genre = models.CharField(max_length=120)
+
 
 class Anime(models.Model):
     name = models.CharField(max_length=300)
     description = models.TextField()
-    genre = models.ForeignKey("Genre", on_delete=models.CASCADE)
+    image = models.ImageField()
+
+
+class AnimeGenre(models.Model):
+    anime = models.ManyToManyField("Anime")
+    genre = models.ManyToManyField("Genre")
+
+
+class Jumbotron(models.Model):
+    anime = models.ForeignKey("Anime", on_delete=models.PROTECT)
+    jumbotron_image = models.ImageField()
 
