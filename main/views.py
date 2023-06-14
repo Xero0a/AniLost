734ac01ab983294332fdc4e166c95dbfe0e9f000
user <chipsikchannel@gmail.com>
2023-06-14 13:59:57 +0300

    Добавлена пагинация.

--- main/views.py
+++ main/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import ListView
+from django.core.paginator import Paginator
 from .models import Genre, Anime
 
 
-class IndexView(View):
+class IndexView(ListView):
     """Отображает главную страницу сайта"""
-
+    
     def get(self, request):
         
         animes = Anime.objects.all()
-        
-        return render(request, "main/index.html", {"animes": animes})
+        paginator = Paginator(animes, 1)
+
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
+        return render(request, "main/index.html", {"page_obj": page_obj})
 
 
 class AnimeView(View):
